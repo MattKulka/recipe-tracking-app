26a22ea8ef654173ffb5566c3b40cf7f1d0b2e81
Matt Kulka <92747257+MattKulka@users.noreply.github.com>
2023-10-30 14:12:50 -0400

    Add files via upload

---
+++ Project_Recipe_App/src/App.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import RecipeCreate from "./RecipeCreate";
+import RecipeList from "./RecipeList";
+import RecipeData from "./RecipeData";
+import "./App.css"
+
+function App() {
+  const [recipes, setRecipes] = useState(RecipeData);
+
+  const createRecipe = (newRecipe) => {
+    setRecipes([...recipes, newRecipe]);
+  };
+
+  const deleteRecipe = (id) => {
+    setRecipes(recipes.filter((recipe) => recipe.id !== id));
+  };
+
+  return (
+    <div className="App">
+      <header>
+        <h1 >Delicious Food Recipes</h1>
+      </header>
+      
+      <RecipeList recipes={recipes} deleteRecipe={deleteRecipe} />
+      
+      <RecipeCreate createRecipe={createRecipe} />
+    </div>
+  );
+}
+
+export default App;
